88986163ef385d3b1bf68b30c4399fb511bde6b7
Vanisper <273266469@qq.com>
2024-09-28 14:08:04 +0800

    init: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import uniHelper from '@uni-helper/eslint-config'
+
+export default uniHelper({
+  ignores: ['dist', 'node_modules', 'public'],
+})
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import uni from '@dcloudio/vite-plugin-uni'
+import Components from '@uni-helper/vite-plugin-uni-components'
+import { WotResolver } from '@uni-helper/vite-plugin-uni-components/resolvers'
+import { defineConfig } from 'vite'
+
+export default defineConfig(async () => {
+  const UnoCSS = (await import('unocss/vite')).default
+
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    plugins: [
+      uni(),
+      UnoCSS(),
+      Components({
+        dts: true,
+        resolvers: [WotResolver()],
+      }),
+    ],
+    build: {
+      target: 'es6',
+      cssTarget: 'chrome61',
+    },
+    optimizeDeps: {
+      exclude: [
+        'vue-demi',
+      ],
+    },
+  }
+})
