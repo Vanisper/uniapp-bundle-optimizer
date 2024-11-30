7b2af2fd99fdcdbef9206a98aa73cc0d93c87705
Vanisper <273266469@qq.com>
2024-11-30 21:41:08 +0800

    feat(src/pages-sub-async): 子包页面更新测试样例

---
+++ src/pages-sub-async/async-plugin/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+export function AsyncPlugin() {
+  console.log(111)
+  return {
+    name: 'async-plugin',
+  }
+}
--- src/pages-sub-demo/index.vue
+++ src/pages-sub-demo/index.vue
@@ -1,9 +1,27 @@
+<!-- eslint-disable no-console -->
 <script lang="ts" setup>
 import AsyncComponentDemo from '@/pages-sub-async/async-component/index.vue'
-import _ from 'lodash'
-import { defineComponent, ref } from 'vue'
+// import { groupBy } from 'lodash' // 这种全量引入到子包中
+// import _ from 'lodash' // 主包会触发 `getDefaultExportFromCjs` 方法的生成，其他表现与上面的引入方式一致
+
+// import groupBy from 'lodash/groupBy' // 按需引入，但是当前`groupBy`内部引入的依赖会被引入到主包中，TODO: 目前需要优化的地方
+import groupBy from 'lodash-es/groupBy'
+import { defineComponent, onMounted, ref } from 'vue'
 
 const someData = ref(Array.from({ length: 10 }, (_, i) => ({ id: i, name: `name-${i}` })))
+
+onMounted(async () => {
+  await import('@/pages-sub-async/async-plugin/index').then((res) => {
+    console.log('plugin', res)
+  })
+
+  AsyncImport('@/pages-sub-async/async-plugin/index').then((res) => {
+    console.log(res?.AsyncPlugin())
+  })
+  AsyncImport('@/pages-sub-async/index.vue').then((res) => {
+    console.log(res.default)
+  })
+})
 </script>
 
 <script lang="ts">
@@ -25,6 +43,6 @@ export default defineComponent({
     <view>---- 对象数组 ---</view>
     <view>{{ someData }}</view>
     <view>---- lodash groupBy ---</view>
-    <view>{{ _.groupBy(someData, 'id') }}</view>
+    <view>{{ groupBy(someData, 'id') }}</view>
   </view>
 </template>
